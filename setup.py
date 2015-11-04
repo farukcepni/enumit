c7dfd11e8729f707f9c7bdc5aff201e3f4b0995f
Faruk Cepni <farukcepni@gmail.com>
2015-11-05 00:00:53 +0200

    readme.md moved readme.rst

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='enumit',
-    version='1.0',
+    version='1.1',
     description='Enumeration library for python.',
     long_description=open('README.md', 'r').read(),
     author='Faruk Cepni',
