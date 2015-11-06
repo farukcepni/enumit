aac8d2e4fbbcca185bdced1048114a61098103fb
Faruk Cepni <farukcepni@gmail.com>
2015-11-06 22:43:31 +0200

    some changes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
-
+from setuptools import setup
 
 setup(
     name='enumit',
     version='1.1',
     description='Enumeration library for python.',
-    long_description=open('README.md', 'r').read(),
+    long_description=open('README.rst', 'r').read(),
     author='Faruk Cepni',
     author_email='farukcepni@gmail.com',
+    py_modules=['enumit'],
     url='https://github.com/farukcepni/enumit',
     keywords=['enumeration', 'enum'],
     classifiers=[
